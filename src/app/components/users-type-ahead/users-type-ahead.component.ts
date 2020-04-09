a31a379816f9cbbf7dc116f9f0072d89bc642301
Landon Messmer <landon.messmer2019@gmial.com>
2020-04-09 14:32:11 -0400

    ability to add and remove tester from array

--- src/app/components/users-type-ahead/users-type-ahead.component.ts
+++ src/app/components/users-type-ahead/users-type-ahead.component.ts
@@ -21,11 +21,15 @@ export interface Tester {
 })
 export class UsersTypeAheadComponent {
   public model: string;
+  value = 'Clear me';
+  selectedTesters: Tester[] = [];
   testers: Tester[] = [
     {uid: '23e4as89', email: 'marissa@gmail.com', firstName: 'Marissa', lastName: 'Martucci' },
     {uid: '1987ds9a', email: 'mitchell@gmail.com', firstName: 'Mitchell', lastName: 'Dudley' },
-    {uid: '13574sad', email: 'Kerry@gmail.com', firstName: 'Kerry', lastName: 'Egan' },
-    {uid: '13574sad', email: 'landon@gmail.com', firstName: 'Landon', lastName: 'Messmer'}
+    {uid: '13574sad', email: 'kerry@gmail.com', firstName: 'Kerry', lastName: 'Egan' },
+    {uid: '13574sad', email: 'landon@gmail.com', firstName: 'Landon', lastName: 'Messmer'},
+    {uid: '95we2fe3', email: 'nick@gmail.com', firstName: 'Nicholas', lastName: 'Newman' },
+    {uid: 'abc3846e', email: 'will@gmail.com', firstName: 'William', lastName: 'Zurita'}
   ];
 
   formatter = (result: Tester) => result.firstName + " " +  result.lastName;
@@ -37,4 +41,16 @@ export class UsersTypeAheadComponent {
     map(term => term === '' ? []
       : this.testers.filter(v => v.firstName.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10)),
   )
+
+  // ADD THE TESTER TO CHIP LIST & REMOVE FROM TABLE
+  addTester(tester: Tester){
+    this.selectedTesters.push(tester);
+    console.log(tester);
+    this.testers = this.testers.filter(x => x != tester);
+  }
+  // REMOVE THE TESTER TO CHIP LIST & ADD FROM TABLE
+  removeTester(tester: Tester){
+    this.selectedTesters = this.selectedTesters.filter(x => x != tester);
+    this.testers.push(tester);
+  }
 }
