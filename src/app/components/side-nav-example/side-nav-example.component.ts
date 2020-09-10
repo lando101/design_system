1ddc897c339ebc17f724f43f71ba8202082b3c3b
Landon Messmer <lmessmer@primrosenet.net>
2020-09-10 13:09:01 -0400

    if no data for dialog use this data

--- src/app/components/side-nav-example/side-nav-example.component.ts
+++ src/app/components/side-nav-example/side-nav-example.component.ts
@@ -13,6 +13,37 @@ export class SideNavExampleComponent implements OnInit {
   ngOnInit(): void {
     console.log(this.sideNavData);
     console.log('Side nav got the data');
+
+    if(!this.sideNavData){
+      // IF NO DATA IS RECEIVED USE THIS DATA INSTEAD :: USED IN FULL SCREEN VIEW
+      this.sideNavData = {
+        caseId: 'Case ID',
+        caseName: 'Case Name',
+        icon: 'far fa-star',
+        tabs:[
+          {tabName: 'Comments', count: 5, order: 0, href: 'comments'},
+          {tabName: 'History', order: 1, href: 'history'},
+          {tabName: 'Details', order: 2, href: 'details'},
+        ],
+        strdMeta: [
+          {desc: 'Badge Label', content:'Status', important: false, badge: true, layout: 'inline', class: 'pimary', type: 'standard'},
+          {desc: 'Label 1', content:'Lorem ipsum', important: false, badge: false, layout: 'inline', type: 'standard'},
+          {desc: 'Label 2', content:'Lorem ipsum', important: false, badge: false, layout: 'inline', type: 'standard'},
+        ],
+        imprtMeta: [
+          {desc: 'Desc Label', content:'Important info', important: true, badge: false, layout: 'block', type: 'highlight'}
+        ],
+        longMeta: [
+          {desc: 'Long Text Label',
+            content:'Cillum ad ut irure tempor velit nostrud occaecat ullamco aliqua anim Lorem sint.',
+            important: false, badge: false, layout: 'block', type: 'paragraph'
+          }
+        ],
+        actionButtons: [
+          {id: 'actions', label: 'Actions', dropDown: true, order: 0},
+        ]
+      };
+    }
   }
 
 }
